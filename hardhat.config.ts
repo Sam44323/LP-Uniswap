55ab187fc07ac99bf9f5fb8a3949340c6ca2fde5
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-09 16:01:26 +0530

    aadd: added custom network configs for hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,20 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
