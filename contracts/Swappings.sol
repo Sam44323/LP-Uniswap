a43f4e7d735d1b557d35799683864e4af2772ad7
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-10 00:02:43 +0530

    update: updated the docs

--- contracts/Swappings.sol
+++ contracts/Swappings.sol
@@ -7,7 +7,7 @@ import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 contract Swappings{
   ISwapRouter public immutable swapRouter;
 
-    // assumed token-hardcoded pools for swapping
+    // assumed token-hardcoded pools for swappings
     address public constant DAI = 0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa;
     address public constant LP = 0x75975fCd5D4bf763F9106b37E64eAC7f54323f7f;
     address public constant LINK = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
