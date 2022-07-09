02053c512d076784e186d78c01d2d76cf1a4ed85
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-09 16:12:31 +0530

    aadd: added custom scripts for runner

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,35 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
-async function main() {}
+async function main() {
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy(
+    "LP Token",
+    "LP",
+    ethers.utils.parseEther("10000")
+  );
+
+  try {
+    console.log("Verifying Token...");
+    await run("verify:verify", {
+      address: token.address,
+      contract: "contracts/Token.sol:Token",
+      constructorArguments: [token.address],
+    });
+  } catch (e: any) {
+    console.error(e.message);
+  }
+
+  console.log("Token deployed to: ", token.address);
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => {
+    console.log("Deployed all contracts...");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
